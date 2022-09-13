1177512f2759a546c08f7351f8dfaa46d357260d
JonPekkonen <jon.pekkonen@zaibatsu.fi>
2022-09-13 11:29:45 +0300

    Initial React project with base file structure

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # react-point-and-click-proto
+Point and click game prototype created with React
+
+## Installation
+
+```
+npm install
+```
+
+## Developing / debugging
+
+```
+npm start
+```
+
+## Building for production
+
+```
+npm run build
+```
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="appHeader">
+        <h2>React point & click prototype</h2>
+      </header>
+      <div className='gameContainer'>
+
+      </div>
+      <div className='footerContainer'>
+        <button>Go back</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
