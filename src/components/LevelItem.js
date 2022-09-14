3028cf9390ba89d1dd1fc55ae351ecc4ef73c1d2
JonPekkonen <jon.pekkonen@zaibatsu.fi>
2022-09-14 15:25:33 +0300

    Image color tint for items on hover Cleaned app.css Addedd gif item as test for gif support

--- src/components/LevelItem.js
+++ src/components/LevelItem.js
@@ -19,10 +19,25 @@ const LevelItem = (props) => {
     }
 
     const mouseOver = (event) => {
-        event.target.style.background = 'rgba(255, 255, 0, 0.5)';
+        /* Various filters for different tints:
+        Normal colors:  'sepia(0%) saturate(100%) brightness(100%) hue-rotate(0deg)';
+        Yellow: 'sepia(100%) saturate(300%) brightness(100%) hue-rotate(0deg)';
+        White/greyscale: 'sepia(100%) saturate(0%) brightness(100%) hue-rotate(0deg)'
+        */
+        if (itemImageSource) {
+            // For separate images use filter to tint image to desired color, colored pixels in image are tinted
+            event.target.style.filter = 'sepia(100%) saturate(0%) brightness(100%) hue-rotate(0deg)';
+        } else {
+            // For area highlights use background color, colored rectangle
+            event.target.style.background = 'rgba(255, 255, 255, 0.4)';
+        }
     }
     const mouseOut = (event) => {
-        event.target.style.background = "";
+        if (itemImageSource) {
+            event.target.style.filter = 'sepia(0%) saturate(100%) brightness(100%) hue-rotate(0deg)';
+        } else {
+            event.target.style.background = 'rgba(0, 0, 0, 0)';
+        }
     }
 
     return (
